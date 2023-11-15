8aff7f3d69ae36f6a74914753db3c4a52a42e2f4
soulgyk <38346065+soulgyk@users.noreply.github.com>
2023-11-15 15:12:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ cusine = st.sidebar.selectbox("Pick a Cusine", ("Indian", "Arabic", "Mexican", "
 
 if cusine:
     response = langchain_helper.generate_restaurant_name_and_items(cusine)
-    st.header(response['restaurant_name'])
-    menu_items = response['menu_items'].split(',')
+    st.header(response['restaurant_name']. strip())
+    menu_items = response['menu_items'].strip().split(',')
     st.write("**Menu Items**")
     for item in menu_items:
         st.write("-", item)
+
 
